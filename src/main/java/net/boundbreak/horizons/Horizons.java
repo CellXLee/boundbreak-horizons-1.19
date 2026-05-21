026eb7a7110bf2ceab0c920d21874994f43ba789
CellXLee <leeleecheetah@gmail.com>
2026-05-21 01:15:41 -0500

    initial commit

--- src/main/java/net/boundbreak/horizons/Horizons.java
+++ src/main/java/net/boundbreak/horizons/Horizons.java
@@ -29,7 +29,7 @@ public class Horizons
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
+        LOGGER.info("Hwewewewewe");
         LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
     }
 
